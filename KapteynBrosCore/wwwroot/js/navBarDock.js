93c9ae79a26b7343a63910862d85752ca03a03a3
Brad <bjkapteyn@gmail.com>
2020-06-16 19:05:11 -0400

    finished sticky navbar

---
+++ KapteynBrosCore/wwwroot/js/navBarDock.js
@@ -0,0 +1,27 @@
+﻿let navElements = {
+    navBarContainer: document.getElementById('navContainer')
+}
+
+function dock() {
+    navElements.navBarContainer.style.right = '0%';
+}
+
+function unDock() {
+    navElements.navBarContainer.style.right = '41%';
+}
+
+function dockUnDock() {
+    debugger;
+    console.log(window.pageYOffset);
+    if (window.pageYOffset >= 80) {
+        unDock();
+    }
+    else {
+        dock();
+    }
+}
+
+window.onscroll = function () {
+
+    dockUnDock();
+}
