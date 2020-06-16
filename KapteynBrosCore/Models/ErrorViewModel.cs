fabcf46983b8adc9d9af76f3b883a76ef0120e11
Brad <bjkapteyn@gmail.com>
2020-06-16 13:26:39 -0400

    migration complete first commit

---
+++ KapteynBrosCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KapteynBrosCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
