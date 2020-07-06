e78d73216c235dcb2052a277503f91a57e1daf5b
Brad <bjkapteyn@gmail.com>
2020-07-06 17:57:18 -0400

    added nav bar effects when scrolling

--- KapteynBrosCore/wwwroot/js/navBarDock.js
+++ KapteynBrosCore/wwwroot/js/navBarDock.js
@@ -1,17 +1,21 @@
 ﻿let navElements = {
-    navBarContainer: document.getElementById('navContainer')
+    navBarContainer: document.getElementById('navContainer'),
+    navBar: document.getElementById('navBar')
 }
 
 function dock() {
+    navElements.navBar.style.transition = 'box-shadow 10ms';
     navElements.navBarContainer.style.right = '0%';
+    navElements.navBar.style.boxShadow = '0px 0px 0px grey';
 }
 
 function unDock() {
+    navElements.navBar.style.transition = 'box-shadow 500ms ease-out';
     navElements.navBarContainer.style.right = '41%';
+    navElements.navBar.style.boxShadow = '5px 0px 10px grey';
 }
 
 function dockUnDock() {
-    debugger;
     console.log(window.pageYOffset);
     if (window.pageYOffset >= 80) {
         unDock();
